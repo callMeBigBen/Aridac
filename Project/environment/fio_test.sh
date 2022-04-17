3d5d10c15f53c5f1e2469fdc9e8c1a181ce7747b
Ubuntu <Aridac@Aridac.ixumd0ipnoyezeezld0qfvi4eg.xx.internal.cloudapp.net>
2022-04-17 17:29:53 -0400

    Update docker image.

---
+++ Project/environment/fio_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir /test_container
+cd /test_container
+
+echo "Randon Read Test..."
+fio --name=randread --directory=/test_container --ioengine=libaio --iodepth=16 --rw=randread --bs=4k --direct=1 --size=256M --numjobs=1 --runtime=36000 --group_reporting
+sleep 10
+rm /test_container/randread.0.0
+
+echo "Randon Write Test..."
+fio --name=randwrite --directory=/test_container --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=1 --size=256M --numjobs=1 --runtime=36000 --group_reporting
+sleep 10
+rm /test_container/randwrite.0.0
+
+cd ..
--- Project/environment/run.sh
+++ Project/environment/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# docker build -t fio_test:v2 .
+
+sudo docker run -it --device-write-bps /dev/sdb:10mb --device-read-bps /dev/sdb:10mb --mount src=/mnt,target=/test_container,type=bind fio_test:v2 bash
 
 
-sudo docker run -it --device-write-bps /dev/sdb:10mb --device-read-bps /dev/sdb:10mb --mount src=/mnt,target=/test_container,type=bind fio_test:v1 bash
