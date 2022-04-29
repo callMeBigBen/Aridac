a7b28014e766b0976c8b070bf44f469d4b630489
Xuan Peng <xuanpeng@localhost>
2022-04-29 11:18:39 -0400

    add docker installation(if not installed)

--- Project/environment/run.sh
+++ Project/environment/run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+# set http & https proxy
+export http_proxy=http://proxy.pdl.cmu.edu:3128/
+export https_proxy=http://proxy.pdl.cmu.edu:3128/
+
+# change to root
+sudo su
+
+# set docker proxy
+# https://docs.docker.com/config/daemon/systemd/#httphttps-proxy
+
+apt-get install docker.io || true
 docker build -t fio_test:v2 .
 
-sudo docker run -it --device-write-bps /dev/sda:10mb --device-read-bps /dev/sda:10mb --mount src=/mnt,target=/test_container,type=bind fio_test:v2 bash
-
+docker run -it --device-write-bps /dev/sda:10mb --device-read-bps /dev/sda:10mb --mount src=/users/xuanpeng/,target=/test_container,type=bind fio_test:v2 bash
 
