b8977e9a3d4f3215b3bc764a2cf29494940e8398
Blickwinkel1107 <1196061706@qq.com>
2022-04-29 12:52:49 -0400

    reader public interfaces

--- Project/src/limiter_reader.py
+++ Project/src/limiter_reader.py
@@ -1,21 +1,26 @@
-import math
 import os
 import sys
 
-def get_quota(path):
+CONTAINER_BASE_DIR = '/sys/fs/cgroup/blkio/docker/'
 
-
-if __name__ == '__main__':
-    base_dir = '/sys/fs/cgroup/blkio/docker/'
-    files = os.listdir(base_dir)
+def get_containers_dirs(argv):
+    files = os.listdir(CONTAINER_BASE_DIR)
     container_dirs = [ f for f in files if not os.path.isfile(f) ]
     match_dirs = []
-    if len(sys.argv) == 0:
+    if len(argv) == 1:
         # list all
         match_dirs = container_dirs
     else:
         # list 1
-        match_dirs = [ f for f in files if f.startswith(sys.argv) ]
+        match_dirs = [ f for f in files if f.startswith(argv[1]) ]
+    return match_dirs
+
+def get_quota(dir):
+    path = CONTAINER_BASE_DIR + dir
+    print(path)
+
+if __name__ == '__main__':
+    match_dirs = get_containers_dirs(sys.argv)
     for d in match_dirs:
         get_quota(d)
 
