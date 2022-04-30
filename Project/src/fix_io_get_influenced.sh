c75e977cac0ade9f96bd3a6aac468af875dbc0cd
Blickwinkel1107 <1196061706@qq.com>
2022-04-30 16:21:02 -0400

    workload1

---
+++ Project/src/fix_io_get_influenced.sh
@@ -0,0 +1,7 @@
+# 1. launch a fio docker A w/ fixed 200kb/s write rate, run for <DP> sec
+# 2. launch a fio docker B w/ 15 threads & no-limit write rate
+# 3. A will get influenced (if no proper policy)
+
+sudo docker run -dit --rm --device-write-bps /dev/sda:10mb --device-read-bps /dev/sda:10mb --mount src=/mnt,target=/test_container,type=bind fio_test:v3 sh -c "./fio_write.sh 128 200 1 1"
+sleep 15
+sudo docker run -dit --rm --device-write-bps /dev/sda:10mb --device-read-bps /dev/sda:10mb --mount src=/mnt,target=/test_container,type=bind fio_test:v3 sh -c "./fio_write.sh 10 10000 15 1"
