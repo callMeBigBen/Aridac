6dfb045758c67cf68c01e1afb0dd1456a9088304
callMeBigBen <thkwrldletsgo@gmail.com>
2022-04-30 15:39:08 -0400

    finish testing stats.py

--- Project/src/stats.py
+++ Project/src/stats.py
@@ -10,8 +10,8 @@ def current_milli_time():
     return float(round(time.time() * 1000))
 
 # 1. probe the maximum throughput for DISK I and O (MB/s)
-MAX_DISK_IPS = 185
-MAX_DISK_OPS = 40
+MAX_DISK_IPS = 2
+MAX_DISK_OPS = 1
 
 def cal_iratio(thruput_delta, time_delta):
     return (thruput_delta* (1000.0/time_delta)) / float(MAX_DISK_IPS)
@@ -42,6 +42,10 @@ name_to_ratio_o = {}
 cur_line = 0
 cur_containers = 0
 tmp_lines = []
+# prev_changed_map_i = {}
+# prev_changed_map_o = {}
+prev_time_map_i = {}
+prev_time_map_o = {}
 
 stats_log_r = open('docker_stats', 'a+')
 time.sleep(1)
@@ -55,16 +59,22 @@ while True:
     while True:
         line = stats_log_r.readline()
         if not line:
-            cur_containers = len(tmp_lines)
             break
         tmp_lines.append(line)
-    
+
+    if now == prev: # abandon the transient points
+        continue
     
     # 6. calculate the ratio with new round of input
     for line in tmp_lines:
+        # print(line)
         elements = line.split()
+        if len(elements) < 10 :
+            continue
         container_id = elements[0]
-        if  elements[-4].endswith('KB'):
+        if container_id == "CONTAINER":
+            continue
+        if  elements[-4].endswith('kB'):
             elements[-4] = elements[-4][:len(elements[-4]) - 2]
             elements[-4] = float(elements[-4]) / 1024.0
         elif  elements[-4].endswith('MB'):
@@ -75,12 +85,16 @@ while True:
         elif  elements[-4].endswith('B'):
             elements[-4] = elements[-4][:len(elements[-4]) - 1]
             elements[-4] = float(elements[-4]) / (1024.0 * 1024.0)
-        acc_block_i = float(elements[-4])
+        try:
+            acc_block_i = float(elements[-4])
+        except ValueError:
+            continue
+        
 
         # if  elements[-2].endswith('B'):
         #     elements[-2] = elements[-2][:len(elements[-2]) - 2]
         #     elements[-2] = float(elements[-2]) / 1024.0
-        if  elements[-2].endswith('KB'):
+        if  elements[-2].endswith('kB'):
             elements[-2] = elements[-2][:len(elements[-2]) - 2]
             elements[-2] = float(elements[-2]) / 1024.0
         elif  elements[-2].endswith('MB'):
@@ -91,7 +105,10 @@ while True:
         elif  elements[-2].endswith('B'):
             elements[-2] = elements[-2][:len(elements[-2]) - 1]
             elements[-2] = float(elements[-2]) / (1024.0 * 1024.0)
-        acc_block_o = float(elements[-2])
+        try:
+            acc_block_o = float(elements[-2])
+        except ValueError:
+            continue
 
         if container_id in prev_name_to_i:
             prev_acc_block_i = prev_name_to_i[container_id]
@@ -110,27 +127,44 @@ while True:
         # process disk input
         if container_id in name_to_ratio_i:
             history_list = name_to_ratio_i[container_id]
-            new_ratio_i = cal_iratio(block_i, now - prev)
-            history_list.append(new_ratio_i)
+            if container_id in prev_time_map_i:
+                new_ratio_i = cal_iratio(block_i, now - prev_time_map_i[container_id])
+            else:
+                new_ratio_i = cal_iratio(block_i, now - prev)
+                prev_time_map_i[container_id] = prev
+            if new_ratio_i > 0 and new_ratio_i <= 1:
+                history_list.append(new_ratio_i)
             if len(history_list) > 50:
                 history_list.pop(0)
         else:
             name_to_ratio_i[container_id] = []
             new_ratio_i = cal_iratio(block_i, now - prev)
-            name_to_ratio_i[container_id].append(new_ratio_i)
+            if new_ratio_i > 0 and new_ratio_i <= 1:
+                name_to_ratio_i[container_id].append(new_ratio_i)
         # process disk output
         if container_id in name_to_ratio_o:
             history_list = name_to_ratio_o[container_id]
-            new_ratio_o = cal_oratio(block_o, now - prev)
-            history_list.append(new_ratio_o)
+            if container_id in prev_time_map_o:
+                new_ratio_o = cal_oratio(block_o, now - prev_time_map_o[container_id])
+            else:
+                new_ratio_o = cal_oratio(block_o, now - prev)
+                prev_time_map_o[container_id] = prev
+            if new_ratio_o > 0 and new_ratio_o <= 1:
+                history_list.append(new_ratio_o)
             if len(history_list) > 50:
                 history_list.pop(0)
         else:
             name_to_ratio_o[container_id] = []
             new_ratio_o = cal_oratio(block_o, now - prev)
-            name_to_ratio_o[container_id].append(new_ratio_o)
+            if new_ratio_o > 0 and new_ratio_o <= 1:
+                name_to_ratio_o[container_id].append(new_ratio_o)
+        if block_o > 0.0:
+            prev_time_map_o[container_id] = now
+        if block_i > 0.0:
+            prev_time_map_i[container_id] = now
 
     print(name_to_ratio_i)
     print(name_to_ratio_o)
+    tmp_lines = []
     # 7. pass the ratio map to policy
     # policy.selector(containers, name_to_ratio_i, name_to_ratio_o)
