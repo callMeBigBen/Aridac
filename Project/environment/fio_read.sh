253f3cb833282801f10e8cabe40b446522ef736d
callMeBigBen <thkwrldletsgo@gmail.com>
2022-04-30 12:03:31 -0400

    add rate control for fio_read.sh & fio_write.sh

--- Project/environment/fio_read.sh
+++ Project/environment/fio_read.sh
@@ -4,7 +4,7 @@ mkdir /test_container
 cd /test_container
 
 echo "Randon Read Test..."
-fio --name=randread --directory=/test_container --ioengine=libaio --iodepth=16 --rw=randread --bs=4k --direct=1 --size="$1"M --numjobs="$2" --runtime=36000 --group_reporting
+fio --name=randread --directory=/test_container --ioengine=libaio --iodepth=16 --rw=randread --bs=4k --direct=1 --size="$1"M --rate="$2"k --numjobs="$3" --runtime=36000 --group_reporting
 sleep 10
 rm /test_container/randread.**
 
