773c9aa244396b6b5f6c2aee4ea215345088c562
callMeBigBen <thkwrldletsgo@gmail.com>
2022-04-30 14:39:44 -0400

    add rate & prio for fio scripts

--- Project/environment/fio_write.sh
+++ Project/environment/fio_write.sh
@@ -4,7 +4,7 @@ mkdir /test_container
 cd /test_container
 
 echo "Randon Write Test..."
-fio --name=randwrite --directory=/test_container --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=1 --size="$1"M --rate="$2"k --numjobs="$3" --runtime=36000 --group_reporting
+fio --name=randwrite --directory=/test_container --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=1 --size="$1"M --rate="$2"k --numjobs="$3" --cmdprio="$4" --runtime=36000 --group_reporting
 sleep 10
 rm /test_container/randwrite.**
 
