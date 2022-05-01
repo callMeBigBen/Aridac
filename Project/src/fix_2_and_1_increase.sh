ad7320b95f44bbd1120285558bbb6e4d5db6d7a7
Blickwinkel1107 <1196061706@qq.com>
2022-05-01 14:17:16 -0400

    workload2

--- Project/src/fix_2_and_1_increase.sh
+++ Project/src/fix_2_and_1_increase.sh
@@ -1,6 +1,6 @@
 # 1. launch a fio docker A & B w/ fixed 10MB/s write rate, run for <DP> sec
-# 2. increase a fio docker B w/ 15 threads & no-limit write rate
+# 2. increase a fio docker B w/ no-limit write rate
 # 3. A will get influenced (if no proper policy)
 
-sudo docker run -dit --rm --device-write-bps /dev/sda:10mb --device-read-bps /dev/sda:10mb --mount src=/mnt,target=/test_container,type=bind fio_test:v3 sh -c "./fio_write.sh 512 10240 1 1"
+sudo docker run -dit --rm --device-write-bps /dev/sda:10mb --device-read-bps /dev/sda:10mb --mount src=/mnt,target=/test_container,type=bind fio_test:v3 sh -c "./fio_write.sh 5120 10240 1 1"
 sudo docker run -dit --rm --device-write-bps /dev/sda:10mb --device-read-bps /dev/sda:10mb --mount src=/mnt,target=/test_container,type=bind fio_test:v3 sh -c "./increase.sh"
